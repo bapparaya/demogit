2ea0d960539de03dddcac7c26ea942ef2d035bce
BAPPARAYA R <BAPPARAYA R@LAPTOP-UBQPUFNI>
2023-03-29 12:24:42 +0530

    firstcommit

---
+++ src/main/java/com/qa/pages/Homepage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class Homepage {
+
+	
+	public void ptint() {
+		System.out.println("this is homepage");
+	}
+}
---
+++ src/test/java/com/qa/testcases/Homepagetest.java
@@ -0,0 +1,14 @@
+package com.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class Homepagetest {
+	
+	
+	@Test
+	public void test()
+	{
+		System.out.println("wwww");
+	}
+
+}
